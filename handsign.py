d0f8bbf9510313e8dcf6d8e96e240e51b159dee2
Ale <alejandro.neff@utexas.edu>
2022-04-26 14:48:38 -0500

    small spaces

--- handsign.py
+++ handsign.py
@@ -36,37 +36,36 @@ while True:
   if cv2.waitKey(1) == ord('q'):
     break
 
-# release the webcam and destroy all active windows
+  framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Get hand landmark prediction
+  result = hands.process(framergb)
+
+  className = ''
+
+  # post process the result
+  if result.multi_hand_landmarks:
+    landmarks = []
+    for handslms in result.multi_hand_landmarks:
+      for lm in handslms.landmark:
+          # print(id, lm)
+          lmx = int(lm.x * x)
+          lmy = int(lm.y * y)
+
+          landmarks.append([lmx, lmy])
+
+      # Drawing landmarks on frames
+      mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+      # REPLACE WITH NEAREST NEIGHBOR CHECKS BASED ON POSITIONS
+
+      # # Predict gesture in Hand Gesture Recognition project
+      # prediction = model.predict([landmarks])
+      # print(prediction)
+      # classID = np.argmax(prediction)
+      # className = classNames[classID]
+
+  # show the prediction on the frame
+  # cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+  # release the webcam and destroy all active windows
 cap.release()
 cv2.destroyAllWindows()
-
-framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-  # Get hand landmark prediction
-result = hands.process(framergb)
-
-className = ''
-
-# post process the result
-if result.multi_hand_landmarks:
-  landmarks = []
-  for handslms in result.multi_hand_landmarks:
-    for lm in handslms.landmark:
-        # print(id, lm)
-        lmx = int(lm.x * x)
-        lmy = int(lm.y * y)
-
-        landmarks.append([lmx, lmy])
-
-    # Drawing landmarks on frames
-    mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-
-# REPLACE WITH NEAREST NEIGHBOR CHECKS BASED ON POSITIONS
-
-# # Predict gesture in Hand Gesture Recognition project
-# prediction = model.predict([landmarks])
-# print(prediction)
-# classID = np.argmax(prediction)
-# className = classNames[classID]
-
-# show the prediction on the frame
-# cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
