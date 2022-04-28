7ca4eeac49fdfaddf60db9cd1e61dcca082314b7
Ale <alejandro.neff@utexas.edu>
2022-04-28 16:52:36 -0500

    live camera works and predicts, uses ratio test too

--- cluster.py
+++ cluster.py
@@ -1,14 +1,7 @@
-#imports
-# import readFaceImages
 import numpy as np
-# import matplotlib.pyplot as plt
-# import math
-# from tabulate import tabulate
 from sklearn.cluster import KMeans
 from sklearn.neighbors import KDTree, KNeighborsClassifier
-# from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from classifier import create_descriptor, fake_dists
-
 
 """
 params:
@@ -21,64 +14,28 @@ pred_labels = M-d array containing predicted labels of test images
 This takes in train and test data (20-d descriptors), creates N clusters, 
 and returns the predicted labels of each test image
 """
-def predict_labels(X_train, X_test):
-    num_clusters = len(X_train)
+def build_kmeans(X_train):
+    num_clusters = 5
     kmeans = KMeans(n_clusters=num_clusters, random_state=0, max_iter=300).fit(X_train)
-    pred_labels = kmeans.predict(X_test)
-
     # print(kmeans.labels_)
     # print(pred_labels)
     # print(kmeans.cluster_centers_)
+    return(kmeans)
 
-    return(pred_labels)
+def predict_centroids(X_test, kmodel):
+    print(np.array(X_test).shape)
+    x = np.expand_dims(X_test, axis=0)
+    pred_labels = kmodel.predict(x)
+    print(np.array(pred_labels).shape)
+    return pred_labels
 
-def test_func():
-    X_train = []
-    for i in range(5):
-        X_train.append(create_descriptor(fake_dists()))
+# def test_func():
+#     X_train = []
+#     for i in range(5):
+#         X_train.append(create_descriptor(fake_dists()))
 
-    X_test = []
-    for i in range(20):
-        X_test.append(create_descriptor(fake_dists()))
+#     X_test = []
+#     for i in range(20):
+#         X_test.append(create_descriptor(fake_dists()))
 
-    return(predict_labels(X_train, X_test))
-
-# print(test_func())
-
-    # Fit K neighbors classifier with the histograms and the labels of the training data.
-    # neighbors = KNeighborsClassifier().fit(histograms, train_gs[0])
-
-
-
-
-
-    # Load in testing data, build histograms for the testing data and then use k means classifier to
-    # predict on the testing histograms. This will yield label predictions for every test image.
-
-    # iterate over all test images and create a histogram for each image
-    # X_test = test_D
-    # histograms_test = []
-    # for i in range(len(X_test)):
-    #     image = X_test[i]
-        
-    #     # use NN to assign each SIFT descriptor to a centroid (build histogram)
-    #     distance, indices = tree.query(image, k=1)     
-    #     histogram = np.histogram(indices, bins=np.arange(1, 122))
-    #     histogram = histogram[0].reshape(1,-1)
-    #     histograms_test.append(histogram)
-
-
-
-
-    # labels = []
-    # for i in range(len(histograms_test)):
-    #     labels.append(neighbors.predict(histograms_test[i]))
-
-
-
-
-
-    # matrix = confusion_matrix(test_gs[0], labels)
-    # display = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=neighbors.classes_)
-    # display.plot()
-    # plt.show()
+#     return(predict_centroids(X_train, X_test))
--- handsign.py
+++ handsign.py
@@ -1,19 +1,14 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-
-# import tensorflow as tf
-# from tensorflow.keras.models import load_model
+import classifier
+import static_handsign
+import cluster
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
-
-#NOT USING MODEL
-
-# # Load the gesture recognizer model
-# model = load_model('mp_hand_gesture')
 
 # Load class names
 f = open('gesture.names', 'r')
@@ -23,6 +18,37 @@ print(classNames)
 
 # Initialize the webcam for Hand Gesture Recognition Python project
 cap = cv2.VideoCapture(0)
+
+"""
+Get truth data
+"""
+truth_data = []
+truth_data.append(static_handsign.generate_truth_data("./images/claws")[0])
+truth_data.append(static_handsign.generate_truth_data("./images/frogs")[0])
+truth_data.append(static_handsign.generate_truth_data("./images/gigem")[0])
+truth_data.append(static_handsign.generate_truth_data("./images/gunsup")[0])
+truth_data.append(static_handsign.generate_truth_data("./images/horns")[0])
+truth_descriptors = [classifier.create_descriptor(truth_data[i]) for i in range(len(truth_data))]
+
+# perform clustering
+# kmodel = cluster.build_kmeans(truth_descriptors)
+
+# relabel_data = []
+# relabel_data.append(static_handsign.generate_truth_data("./images/claws")[1])
+# relabel_data.append(static_handsign.generate_truth_data("./images/frogs")[1])
+# relabel_data.append(static_handsign.generate_truth_data("./images/gigem")[1])
+# relabel_data.append(static_handsign.generate_truth_data("./images/gunsup")[1])
+# relabel_data.append(static_handsign.generate_truth_data("./images/horns")[1])
+
+# labels = kmodel.predict([classifier.create_descriptor(relabel_data[i]) for i in range(len(relabel_data))])
+# print(labels)
+# label_alignment = {
+#   labels[0]:"claws",
+#   labels[1]:"frogs",
+#   labels[2]:"gigem",
+#   labels[3]:"gunsup",
+#   labels[4]:"horns"
+# }
 
 while True:
   # Read each frame from the webcam
@@ -43,6 +69,7 @@ while True:
   # post process the result
   if result.multi_hand_landmarks:
     landmarks = []
+    # signs = np.zeros(5)
     for handslms in result.multi_hand_landmarks:
       for lm in handslms.landmark:
           # print(id, lm)
@@ -65,15 +92,21 @@ while True:
       Closest match (use ratio test) is the predicted handsign.
       If failing ratio test, don't assign a handsign label
       """
-
-      # # Predict gesture in Hand Gesture Recognition project
-      # prediction = model.predict([landmarks])
-      # print(prediction)
-      # classID = np.argmax(prediction)
-      # className = classNames[classID]
-
+      new_descriptor = classifier.create_descriptor(landmarks)
+      dists = [classifier.dist_to_target(new_descriptor, truth_descriptors[x]) for x in range(5)]
+      # className = classNames[np.argmin(dists)]
+      sorted_dists = [dists[x] for x in range(5)]
+      sorted_dists.sort()
+      ratio = sorted_dists[0] / sorted_dists[1]
+      if ratio < 0.9:
+        className = classNames[np.argmin(dists)]
+      else:
+        className = ""
+      # signs[np.argmin(dists)] += 1
+    # className = label_alignment[int(cluster.predict_centroids(new_descriptor, kmodel))]
+    # className = ("claws: " + str(signs[0]) + " frogs: " + str(signs[1]) + " gigem: " + str(signs[2]) + " gunsup: " + str(signs[3]) + " horns: " + str(signs[4]))
   # show the prediction on the frame
-  cv2.putText(frame, "Hand", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+  cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
   cv2.imshow("Output", frame)
   if cv2.waitKey(1) == ord('q'):
     break
