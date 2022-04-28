7961b523c5ba64c60a83c62cc5b1f1c95d780ee7
sampippen <samfpippen@utexas.edu>
2022-04-28 15:07:50 -0500

    Created test kmeans cluster func which returns pred labels

--- cluster.py
+++ cluster.py
@@ -5,47 +5,48 @@ import numpy as np
 # import math
 # from tabulate import tabulate
 from sklearn.cluster import KMeans
-# from sklearn.neighbors import KDTree, KNeighborsClassifier
+from sklearn.neighbors import KDTree, KNeighborsClassifier
 # from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from classifier import fake_dists
+from classifier import create_descriptor, fake_dists
 
-def func(descriptors):
-    num_clusters = len(descriptors, X_train)
 
-    descriptors = np.zeros((5, 20))
+"""
+params:
+X_train = N 20-d descriptors for ground truth gestures, values in range [0,1] from landmark detector
+X_test = M 20-d descriptors for test images, values in range [0,1] from landmark detector
 
-    # fit kmeans model to subset of training data
+output:
+pred_labels = M-d array containing predicted labels of test images
+
+This takes in train and test data (20-d descriptors), creates N clusters, 
+and returns the predicted labels of each test image
+"""
+def predict_labels(X_train, X_test):
+    num_clusters = len(X_train)
     kmeans = KMeans(n_clusters=num_clusters, random_state=0, max_iter=300).fit(X_train)
-    clusters = kmeans.cluster_centers_
+    pred_labels = kmeans.predict(X_test)
 
+    print(kmeans.labels_)
+    print(pred_labels)
+    print(kmeans.cluster_centers_)
 
+    return(pred_labels)
 
+def test_func():
+    X_train = []
+    for i in range(5):
+        X_train.append(create_descriptor(fake_dists(), (600, 400, 0)))
 
+    X_test = []
+    for i in range(20):
+        X_test.append(create_descriptor(fake_dists(), (600, 400, 0)))
 
-    # Calculate a histogram for each image in the training set. The K means algorithm
-    # which now knows the centroids, predicts which centroid each SIFT descriptor
-    # belongs to aka perform NN on the centroids.
+    return(predict_labels(X_train, X_test))
 
-
-    # iterate over all train images and create a histogram for each
-    # image by using NN to assign each SIFT descriptor to a centroid
-    histograms = []
-    for i in range(len(X_train)):
-        im = train_D[i]
-        tree = KDTree(clusters)
-        distance, indices = tree.query(im, k=1)   
-        histogram = np.histogram(indices, bins=np.arange(1, 122))
-        histograms.append(histogram[0])
-        
-    # Each training image has a histogram representing roughly how many visual words that image has
-    # of whatever type of visual words there are in the total set of images.
-
-
-
-
+print(test_func())
 
     # Fit K neighbors classifier with the histograms and the labels of the training data.
-    neighbors = KNeighborsClassifier().fit(histograms, train_gs[0])
+    # neighbors = KNeighborsClassifier().fit(histograms, train_gs[0])
 
 
 
@@ -55,23 +56,23 @@ def func(descriptors):
     # predict on the testing histograms. This will yield label predictions for every test image.
 
     # iterate over all test images and create a histogram for each image
-    X_test = test_D
-    histograms_test = []
-    for i in range(len(X_test)):
-        image = X_test[i]
+    # X_test = test_D
+    # histograms_test = []
+    # for i in range(len(X_test)):
+    #     image = X_test[i]
         
-        # use NN to assign each SIFT descriptor to a centroid (build histogram)
-        distance, indices = tree.query(image, k=1)     
-        histogram = np.histogram(indices, bins=np.arange(1, 122))
-        histogram = histogram[0].reshape(1,-1)
-        histograms_test.append(histogram)
+    #     # use NN to assign each SIFT descriptor to a centroid (build histogram)
+    #     distance, indices = tree.query(image, k=1)     
+    #     histogram = np.histogram(indices, bins=np.arange(1, 122))
+    #     histogram = histogram[0].reshape(1,-1)
+    #     histograms_test.append(histogram)
 
 
 
 
-    labels = []
-    for i in range(len(histograms_test)):
-        labels.append(neighbors.predict(histograms_test[i]))
+    # labels = []
+    # for i in range(len(histograms_test)):
+    #     labels.append(neighbors.predict(histograms_test[i]))
 
 
 
